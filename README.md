e56eea3834a75e00d0e339e1c3407274bbb8fd31
orthagonal <orthagonal1@gmail.com>
2021-03-01 11:48:21 -0600

    readme

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # envload
 
-envload is a simple helper that loads a set of environment variables into Javascript
+envload is a simple helper for loading environment variables into Javascript
+
+## Installation
+
+```console
+npm install envload
+```
 
 ## Usage
+
+```console
+export PREFIX_TEST=1
+export PREFIX_FULL_NAME__FIRST="maggie"
+export PREFIX_FULL_NAME__LAST="smith"
+```
+
+```js
+const env = envload('prefix');
+console.log(env);
+
+/*
+{
+  test: 1,
+  fullName: {
+    first: 'bob',
+    last: 'smith'
+  }
+}
+*/
+
+```
+
+You can also pass an env in object notation:
 ```js
 const env = envload('prefix', {
   PREFIX_TEST: 1,
--- test/test.envload.js
+++ test/test.envload.js
@@ -6,8 +6,25 @@ tap.test('can turn an env into a set of properties', (t) => {
   const env = envload('prefix', {
     PREFIX_TEST: 1,
     PREFIX_FULL_NAME__FIRST: 'bob',
-    PREFIX_FULL_NAME__LAST: 'smith'
+    PREFIX_FULL_NAME__LAST: 'smith',
   });
+  console.log(env)
+  t.deepEqual(env, {
+    test: 1,
+    fullName: {
+      first: 'bob',
+      last: 'smith'
+    }
+  });
+  t.end();
+});
+
+
+tap.test('by default will use process.env', (t) => {
+  process.env.PREFIX_TEST = 1;
+  process.env.PREFIX_FULL_NAME__FIRST = 'bob';
+  process.env.PREFIX_FULL_NAME__LAST = 'smith';
+  const env = envload('prefix');
   console.log(env)
   t.deepEqual(env, {
     test: 1,
